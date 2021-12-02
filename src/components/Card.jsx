35bfedcc1f067c34c9a3620380cdf4f4959fcdd8
adham618 <adhamtarek291@gmail.com>
2021-12-02 20:43:10 +0200

    create card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import './SingleCard.css'
+
+const Card = ({ card }) => {
+  return (
+    <div className="card">
+      <div>
+        <img className="front" src={card.src} alt="card front" draggable="false" />
+        <img className="back" src="/img/cover.png" alt="cover" draggable="false" />
+      </div>
+    </div>
+  )
+}
+
+export default Card
