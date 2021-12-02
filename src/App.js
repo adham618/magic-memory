aed328a31278ffd5da38fc76a770024ac3176401
adham618 <adhamtarek291@gmail.com>
2021-12-02 18:52:34 +0200

    replace vite with cra

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react"
+
+const cardImages = [
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/potion-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/sword-1.png" },
+]
+const App = () => {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.floor(Math.random() * 100) }))
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+  console.log(cards, turns)
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card grid">
+        {cards.map((card) => {
+          <div className="card" key={card.id}>
+            <div>
+              <img src={card.src} alt="card front" className="front" />
+              <img src="./img/cover.png" alt="card back" className="back" />
+            </div>
+          </div>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
