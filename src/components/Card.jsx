69baf64190ae984b75b0234ac87a03e11cbc5f16
adham618 <adhamtarek291@gmail.com>
2021-12-03 19:45:52 +0200

    complete cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,20 @@
 import './Card.css'
 
-const Card = ({ card }) => {
+const Card = ({ card, handleChoice, flipped, disabled }) => {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card)
+    }
+  }
   return (
     <div className="card">
-      <div>
+      <div className={flipped ? "flipped" : ""}>
         <img className="front" src={card.src} alt="card front" draggable="false" />
-        <img className="back" src="/img/cover.png" alt="cover" draggable="false" />
+        <img className="back"
+          src="/img/cover.png"
+          alt="cover"
+          draggable="false"
+          onClick={handleClick} />
       </div>
     </div>
   )
